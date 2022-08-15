369e32e3dd656b44c870e89b4882bd4a35b8ff44
Roman Lesnoy <romanlesnoy@outlook.com>
2022-08-15 18:01:22 +0300

    Add fetch images logic

--- src/components/CardList/CardList.jsx
+++ src/components/CardList/CardList.jsx
@@ -1,12 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import classes from "./CardList.module.css";
-import Card from "../Card/Card";
+import ImageCard from "../ImageCard/ImageCard";
+
+import useHttp from "../../hooks/use-http.js";
 
 const CardList = () => {
+    const [images, setImages] = useState([]);
+
+    const { isLoading, error, sendRequest: fetchCards } = useHttp();
+
+    useEffect(() => {
+        const getCards = (cards) => {
+            setImages(cards);
+        };
+
+        fetchCards(
+            {
+                url: "https://boiling-refuge-66454.herokuapp.com/images"
+            },
+            getCards
+        );
+    }, [fetchCards]);
+
+    if (isLoading) {
+        return <p>Loading...</p>;
+    }
+
+    if (error) {
+        return <p>{error}</p>;
+    }
+
     return (
         <ul className={classes.cardList}>
-            <Card />
+            {images &&
+                images.map((image) => {
+                    return <ImageCard key={image.id} url={image.url} />;
+                })}
         </ul>
     );
 };
