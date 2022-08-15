16aae0cc6656ee36f0f54839dcd65d8ff76ce8f2
Roman Lesnoy <romanlesnoy@outlook.com>
2022-08-15 15:25:12 +0300

    Set up linter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import './App.css';
+import React from "react";
+
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header"></header>
+        </div>
+    );
 }
 
 export default App;
