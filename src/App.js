038fcfb332b9a0a232259fc57b29365b62f10aec
Roman Lesnoy <romanlesnoy@outlook.com>
2022-08-18 14:18:48 +0300

    Add React.lazy for Popup component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, Suspense } from "react";
 
+import Preloader from "./components/Preloader/Preloader";
 import CardList from "./components/CardList/CardList";
-import Popup from "./components/Popup/Popup";
 import Banner from "./components/Banner/Banner";
 import Profile from "./components/Profile/Profile";
+
+const Popup = React.lazy(() => import("./components/Popup/Popup"));
 
 function App() {
     const [popupIsShown, setPopupIsShown] = useState(false);
@@ -19,16 +21,21 @@ function App() {
     };
 
     return (
-        <div className="App">
-            {popupIsShown && (
-                <Popup onClose={hidePopupHandler} imageId={selectedImageId} />
-            )}
-            <Banner />
-            <Profile />
-            <main>
-                <CardList onImageClick={handleImage} />
-            </main>
-        </div>
+        <>
+            <Suspense fallback={<Preloader />}>
+                {popupIsShown && (
+                    <Popup
+                        onClose={hidePopupHandler}
+                        imageId={selectedImageId}
+                    />
+                )}
+                <Banner />
+                <Profile />
+                <main>
+                    <CardList onImageClick={handleImage} />
+                </main>
+            </Suspense>
+        </>
     );
 }
 
